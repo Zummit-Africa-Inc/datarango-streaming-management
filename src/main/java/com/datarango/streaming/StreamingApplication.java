e8eef19cf28920f6b2185eb49ad202cab1e03c0a
Samson Okunola <smsnmicheal@gmail.com>
2026-03-05 23:15:02 +0100

    Refactor ServletInitializer functionality into StreamingApplication

--- src/main/java/com/datarango/streaming/StreamingApplication.java
+++ src/main/java/com/datarango/streaming/StreamingApplication.java
@@ -2,6 +2,7 @@ package com.datarango.streaming;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -11,4 +12,8 @@ public class StreamingApplication extends SpringBootServletInitializer {
 		SpringApplication.run(StreamingApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(StreamingApplication.class);
+	}
 }
